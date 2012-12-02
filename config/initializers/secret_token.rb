e548524fe7f65e8e755aee3976b4550dd5058a14
tsuchikazu <huneyhunt77@gmail.com>
2012-12-02 15:18:54 +0900

    twitter boostrap and Devise installed

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Camera::Application.config.secret_token = '118844739f88ba55bb2ef9d825707e260611a274b7f44893008c4de2cb2295804ce13c9a7f60d9b6d34480774cf2701e40ed25f32e409c1e60d3bf1912c2803d'
