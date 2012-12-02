470ef9988e3f38a05ea536375afdc9ae2430a91e
tsuchikazu <huneyhunt77@gmail.com>
2012-12-02 18:04:51 +0900

    devise japaneses translate

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,19 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Camera::Application.initialize!
+
+Camera::Application.configure do
+  # Mail Config
+  config.action_mailer.delivery_method       = :smtp
+  config.action_mailer.perform_deliveries    = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+    :address              => Settings.smtp.address, 
+    :port                 => Settings.smtp.port, 
+    :domain               => Settings.smtp.domain, 
+    :user_name            => Settings.smtp.user_name, 
+    :password             => Settings.smtp.password, 
+    :authentication       => 'plain', 
+    :enable_starttls_auto => true  }
+end
+
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,9 +13,6 @@ Camera::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
-  # Don't care if the mailer can't send
-  config.action_mailer.raise_delivery_errors = false
-
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
 
@@ -34,4 +31,8 @@ Camera::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # device url
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+
 end
